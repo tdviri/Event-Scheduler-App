2c1c06dadf9ca53a1603e9386cf552371750a185
tdviri <tdviri20@gmail.com>
2021-12-17 19:44:11 -0500

    updated navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,23 +2,45 @@ const Navbar = () => {
   return (
     <nav>
       <div
-        className="gridContainer"
+        className="links"
         style={{
+          position: "relative",
           backgroundColor: "gray",
           textAlign: "left",
           width: "100vw",
           height: "7vh",
-          gridTemplateColumns: "repeat(4, 100px)",
-          gridGapColumns: "80px",
-          alignContent: "center",
         }}
       >
-        <a href="/" style={{ marginLeft: "50px" }}>
+        <a
+          href="/"
+          style={{
+            position: "absolute",
+            left: "30px",
+            fontSize: "20px",
+          }}
+        >
           Home
         </a>
-        <a href="/">Registration</a>
-        <a href="/">Schedule Event</a>
-        <a href="/" style={{ alignSelf: "right" }}>
+        <a
+          href="/"
+          style={{ position: "absolute", left: "120px", fontSize: "20px" }}
+        >
+          Registration
+        </a>
+        <a
+          href="/"
+          style={{ position: "absolute", left: "270px", fontSize: "20px" }}
+        >
+          Schedule Event
+        </a>
+        <a
+          href="/"
+          style={{
+            position: "absolute",
+            right: "40px",
+            fontSize: "20px",
+          }}
+        >
           Admin
         </a>
       </div>
--- src/Registration.js
+++ src/Registration.js
@@ -14,7 +14,42 @@ const Registration = () => {
       >
         Registration
       </h1>
-      <MeetingInfo />
+      <div
+        className="gridContainer"
+        style={{
+          gridTemplateRows: "repeat(3, 100px)",
+          gridRowGap: "150px",
+          justifyContent: "left",
+          alignItems: "center",
+        }}
+      >
+        <MeetingInfo />
+        <MeetingInfo />
+        <MeetingInfo />
+      </div>
+      <div
+        id="registrationForm"
+        style={{
+          position: "relative",
+          height: "70vh",
+          width: "30vw",
+          right: "50px",
+          top: "100px",
+        }}
+      >
+        <div style={{ position: "absolute", width: "100%", height: "20%" }}>
+          Event Registration Form
+        </div>
+        <form>
+          <label for="fname">First name:</label>
+          <input type="text" id="fname" name="fname"></input>
+          <label for="lname">Last name:</label>
+          <input type="text" id="lname" name="lname"></input>
+          <input type="submit" value="Submit"></input>
+        </form>
+      </div>
     </div>
   );
 };
+
+export default Registration;
